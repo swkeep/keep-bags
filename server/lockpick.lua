1f2692f16b9261c4a6f4c0f33a0360d53b40c47d
swkeep <badaosoft@gmail.com>
2023-05-14 23:36:49 +0330

    fix(lockpick): Fix whitelist feat(lockpick): Show notification

--- server/lockpick.lua
+++ server/lockpick.lua
@@ -1,27 +1,34 @@
 local Harmony = exports["keep-harmony"]:GetCoreObject()
-local Shared = exports["keep-harmony"]:Shared()
 local CreateUseableItem = Harmony.Item.CreateUseableItem
 
 local function isWhitelisted(Player)
     if not Config.lockpick_whitelist.active then return true end
 
-    local cid = Player.PlayerData.citizenid
-    local jobname = Player.PlayerData.job.name
+    local cid = Harmony.Player.Identifier(Player)
+    local jobname = Harmony.Player.Job(Player)
 
-    for _, w_cid in ipairs(Config.lockpick_whitelist.citizenid) do
-        if w_cid == cid then return true end
+    for _, w_cid in pairs(Config.lockpick_whitelist.citizenid) do
+        if w_cid == cid then return true, 'citizenid' end
     end
 
-    for _, w_job in ipairs(Config.lockpick_whitelist.jobs) do
-        if w_job == jobname then return true end
+    for _, w_job in pairs(Config.lockpick_whitelist.jobs) do
+        if w_job == jobname then return true, 'job' end
     end
 
     return false
 end
 
+local function canUseLockpick(source, Player)
+    if not isWhitelisted(Player) then
+        Harmony.Player.Notify(source, 'You can not use this type of lockpick', 'error')
+        return false
+    end
+    return true
+end
+
 local function use_lockpick(source, item)
     local Player = Harmony.Player.Object(source)
-    if not Player or not isWhitelisted(Player) then return end
+    if not Player or not canUseLockpick(source, Player) then return end
 
     local items = {}
 
@@ -39,10 +46,10 @@ end
 
 Harmony.Event.onNet('server:lockpick:open', function(source, item_name, id)
     local Player = Harmony.Player.Object(source)
+    if not Player or not canUseLockpick(source, Player) then return end
+
     local backpack_item = Harmony.Item.Search_by.Id(Player, item_name, id)
     local hasItemLockpick = Harmony.Player.HasItem(Player, 'briefcaselockpicker')
-
-
     if not IsBackpack(backpack_item) or not hasItemLockpick then return end
 
     if Harmony.Player.RemoveItem(source, Player, 'briefcaselockpicker') then
